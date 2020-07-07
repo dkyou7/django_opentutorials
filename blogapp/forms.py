a6a6c619bbc22baaa8784f431516b733cbfb600b
youdongkwan <dkyou7@naver.com>
2020-07-08 00:12:16 +0900

    글쓰기 페이지 꾸미기

--- blogapp/forms.py
+++ blogapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Blog
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 
 class CreateBlog(forms.ModelForm):
@@ -7,3 +8,13 @@ class CreateBlog(forms.ModelForm):
         model = Blog
 
         fields = ['title', 'author', 'body']
+
+        widgets = {
+            'title': forms.TextInput(
+                attrs={'class': 'form-control', 'style': 'width: 100%', 'placeholder': '제목을 입력하세요.'}
+            ),
+            'author': forms.Select(
+                attrs={'class': 'custom-select'},
+            ),
+            'body': forms.CharField(widget=CKEditorUploadingWidget()),
+        }
