46ba9918c940b7a0e8e797b8d2650806422da5e5
youdongkwan <dkyou7@naver.com>
2020-07-08 00:05:03 +0900

    CKEditor 추가

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 
 # Create your models here.
@@ -7,4 +8,4 @@ class Blog(models.Model):
     title = models.CharField(max_length=100)
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=1)
-    body = models.TextField()
+    body = RichTextField()
