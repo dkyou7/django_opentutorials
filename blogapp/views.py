d18d55e867f416e8163515241fb3fe33a6aec0fd
youdongkwan <dkyou7@naver.com>
2020-07-08 00:35:59 +0900

    글 detail 기능 구현

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,12 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CreateBlog
+from .models import Blog
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def blogMain(request):
-    return render(request, 'blogMain.html')
+    blogs = Blog.objects.all()
+
+    return render(request, 'blogMain.html', {'blogs': blogs})
 
 def createBlog(request):
 
@@ -21,3 +24,8 @@ def createBlog(request):
     else:
         form = CreateBlog()
         return render(request, 'createBlog.html',{'form':form})
+
+def detail(request,blog_id):
+    blog_detail = get_object_or_404(Blog, pk=blog_id)
+
+    return render(request, 'detail.html', {'blog_detail': blog_detail})
