6313bce0b4f37889226b5712c7f467d4d2b49f2f
youdongkwan <dkyou7@naver.com>
2020-07-11 09:29:48 +0900

    소셜로그인 되는데 왜 여기서는 안되죠?

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import CreateBlog
 from .models import Blog, Comment
+from .forms import BlogCommentForm
 
 # Create your views here.
 def index(request):
@@ -25,13 +26,61 @@ def createBlog(request):
         form = CreateBlog()
         return render(request, 'createBlog.html',{'form':form})
 
-def detail(request,blog_id):
+
+def detail(request, blog_id):
     blog_detail = get_object_or_404(Blog, pk=blog_id)
     comments = Comment.objects.filter(blog_id=blog_id)
 
-    context = {
-        'blog_detail': blog_detail,
-        'comments': comments
-    }
+    if request.method == 'POST':
+        comment_form = BlogCommentForm(request.POST)
 
-    return render(request, 'detail.html', context)
+        if comment_form.is_valid():
+            content = comment_form.cleaned_data['comment_textfield']
+
+            print(content)
+
+            login_request_uri = 'https://kauth.kakao.com/oauth/authorize?'
+
+            client_id = '0645221321e0c7c1a53777575a9527cc'
+            redirect_uri = 'http://127.0.0.1:8000/oauth'
+
+            login_request_uri += 'client_id=' + client_id
+            login_request_uri += '&redirect_uri=' + redirect_uri
+            login_request_uri += '&response_type=code'
+
+            request.session['client_id'] = client_id
+            request.session['redirect_uri'] = redirect_uri
+
+
+            return redirect(login_request_uri)
+        else:
+            return redirect('blogMain')
+
+    else:
+        comment_form = BlogCommentForm()
+
+        context = {
+            'blog_detail': blog_detail,
+            'comments': comments,
+            'comment_form': comment_form
+        }
+
+        return render(request, 'detail.html', context)
+
+def oauth(request):
+    code = request.GET['code']
+    print('code = ' + str(code))
+
+    client_id = request.session.get('client_id')
+    redirect_uri = request.session.get('redirect_uri')
+
+    access_token_request_uri = "https://kauth.kakao.com/oauth/token?grant_type=authorization_code&"
+
+    access_token_request_uri += "client_id=" + client_id
+    access_token_request_uri += "&redirect_uri=" + redirect_uri
+    access_token_request_uri += "&code=" + code
+
+    print(access_token_request_uri)
+
+
+    return redirect('blogMain')
